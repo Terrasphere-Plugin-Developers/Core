3fe4d8a7064ab53d1b21bc42b5a292521998156b
Skyswimsky <Skyswim07@gmail.com>
2021-09-02 11:48:37 +0200

    Gave the parsedURL stuff an ID replacer and removed the parsedURLString

--- Entity/BannerButton.php
+++ Entity/BannerButton.php
@@ -30,51 +30,12 @@ class BannerButton extends Entity
     public function getParsedURL($user)
     {
         $url = $this['url'];
-        if(preg_match("/\{player_stats}/i", $url))
+        if(preg_match("/{id}/", $url))
         {
-            $replaceString = $this->getBuildEditorParamsString($user);
-            $url = str_replace("{player_stats}", $replaceString, $url);
+            $replaceString = $user->user_id;
+            $url = str_replace("{id}", $replaceString, $url);
         }
 
         return $url;
-    }
-
-
-    private function getBuildEditorParamsString(User $user) : string
-    {
-        $masteryNames = "";
-        $masteryRanks = "";
-
-        $masteries = $user->getMasteries();
-
-        foreach ($masteries as $mastery)
-        {
-            // Make sure each mastery display name is formatted as expected, using single dashes as word separators.
-            $masteryString = strtolower($mastery->Mastery['display_name']);
-            $masteryString = str_replace(' ', '-', $masteryString);
-            $masteryString = str_replace('_', '-', $masteryString);
-
-            $masteryRanks .= $mastery->Rank['tier'];
-
-            // Add comma separators.
-            $masteryNames .= $masteryString . ',';
-            $masteryRanks .= ',';
-        }
-
-        // Remove last comma for both names and ranks.
-        $masteryNames = substr($masteryNames, 0, -1);
-        $masteryRanks = substr($masteryRanks, 0, -1);
-
-        $weapon = $user->getOrInitiateWeapon();
-        $weaponRank = $this->finder('Terrasphere\Core:Rank')->whereId($weapon['rank_id'])->fetchOne();
-
-        $armor = $user->getOrInitiateArmor();
-        $armorRank = $this->finder('Terrasphere\Core:Rank')->whereId($armor['rank_id'])->fetchOne();
-
-        return $masteryNames . '.' .
-            $masteryRanks .'..' .
-            $weaponRank['tier'] . '.' .
-            $armorRank['tier'] . '.' .
-            str_replace(' ', '_', $user->username);
     }
 }
